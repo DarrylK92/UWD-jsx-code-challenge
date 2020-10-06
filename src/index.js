35df352ba556a2619a4b7d70f601894eb865b6f3
Darryl Karney <DarrylK@cgi1.cgiltd.com>
2020-10-06 13:21:03 -0500

    Initial

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Heading</h1>
+    <ul>
+      <li>
+        <p>First item</p>
+      </li>
+
+      <li>
+        <p>Second item</p>
+      </li>
+
+      <li>
+        <p>Third item</p>
+      </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
